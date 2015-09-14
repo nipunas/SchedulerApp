fb26631ede40c5ae78b6b20ec62f4f519b664a1b
nipunas <nipuna77@gmail.com>
2015-09-14 17:30:27 +0530

    Changes to task service and referring correct controller after renamings

--- SchedulerApp/SchedulerApp/App/Services/TaskService.js
+++ SchedulerApp/SchedulerApp/App/Services/TaskService.js
@@ -1,12 +1,66 @@
 ﻿(function () {
     var module = angular.module('SchedulerApp');
 
-    module.factory('TaskService', function () {
+    module.factory('TaskService', function ($location, HttpService) {
+        function getDuration(duration) {
+            switch (duration) {
+                case 'today':
+                    return 1;
+                case 'week':
+                    return 2;
+                case 'month':
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+
         var tasks = [];
+
+        var task = {
+            Id: -1,
+            Summary: '',
+            Description: '',
+            Completed: false
+        },
+        onTaskDeletionComplete = function (response) {
+            //TODO:
+        },
+        taskModel = function (model) {
+
+            var item = {};
+            item.Id = model.Id;
+            item.Summary = model.Summary;
+            item.Description = model.Description;
+            item.Completed = model.Completed
+
+            return item;
+        },
+        addTask = function (taskData, callback) {
+            var newTask = new taskModel(taskData);
+
+            HttpService.createTask(newTask)
+                .then(callback);
+
+            //We need to push a new task everytime. Otherwise the same task with references
+            //will be added each time you press add task
+            tasks.push(newTask);
+        },
+        deleteTask = function (taskId, callback) {
+            HttpService.deleteTask(taskId)
+            .then(callback);
+        },
+        getTasks = function (duration, userId, callback) {
+            HttpService.getTasks(getDuration(duration), userId)
+            .then(callback);
+        };
 
         return {
             tasks: tasks,
-            tasksLoaded: false
+            tasksLoaded: false,
+            addTask: addTask,
+            deleteTask: deleteTask,
+            getTasks: getTasks
         };
 
     });
