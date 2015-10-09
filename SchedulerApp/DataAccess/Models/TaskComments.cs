0ad55ed38158c930ffaeea79de414a057967a1dd
nipunas <nipuna77@gmail.com>
2015-10-09 07:59:13 +0530

    Adding class details

--- SchedulerApp/DataAccess/Models/TaskComments.cs
+++ SchedulerApp/DataAccess/Models/TaskComments.cs
@@ -8,5 +8,11 @@ namespace DataAccess.Models
 {
     public class TaskComments
     {
+        public int TaskId { get; set; }
+        public int CommentId { get; set; }
+        public bool HasAttachment { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public bool? IsDeleted { get; set; }
     }
 }
