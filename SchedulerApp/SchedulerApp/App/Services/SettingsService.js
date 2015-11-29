1d5db7555484d09e09e442abf0e010c94321b729
nipunas <nipuna77@gmail.com>
2015-11-29 10:21:35 +0530

    Adding new files

---
+++ SchedulerApp/SchedulerApp/App/Services/SettingsService.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var module = angular.module('SchedulerApp');
+
+    module.factory('SettingsService', function () {
+        var loggedIn = false,
+            username = '';
+
+        return {
+            loggedIn: loggedIn,
+            username: username
+        };
+
+    });
+
+}());
