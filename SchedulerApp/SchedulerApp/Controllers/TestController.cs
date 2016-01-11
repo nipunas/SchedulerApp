c3d40ed4dbb411f2efa3529931c6df0914b40667
nipunas <nipuna77@gmail.com>
2016-01-11 18:21:26 +0530

    Adding test controller

---
+++ SchedulerApp/SchedulerApp/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchedulerApp.Controllers
+{
+    public class TestController : Controller
+    {
+        //http://www.asp.net/mvc/overview/older-versions-1/unit-testing/creating-unit-tests-for-asp-net-mvc-applications-cs
+        public string GetValue(int id)
+        {
+            return id + " ad";
+        }
+
+	}
+}
