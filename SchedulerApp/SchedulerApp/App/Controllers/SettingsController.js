36b07e9dcd057fc51b10d2f971873726b71eebf1
nipunas <nipuna77@gmail.com>
2016-01-23 22:59:12 +0530

    Integrating angular controller and model with controller view and adding some test data

--- SchedulerApp/SchedulerApp/App/Controllers/SettingsController.js
+++ SchedulerApp/SchedulerApp/App/Controllers/SettingsController.js
@@ -4,6 +4,10 @@
 
     var SettingsController = function ($scope, HttpService) {
         $scope.a = 1;
+        $scope.userName = 'user42211-S';
+        $scope.password = 'q232';
+
+        
     };
 
     app.controller("SettingsController", ["$scope", SettingsController]);
