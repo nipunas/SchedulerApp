d0c56a797752275180ce2dbba94aa06a093bbe79
nipunas <nipuna77@gmail.com>
2016-01-23 15:05:42 +0530

    Adding new flag to db project

--- SchedulerApp/SchedulerDB/dbo/Tables/User.sql
+++ SchedulerApp/SchedulerDB/dbo/Tables/User.sql
@@ -2,6 +2,9 @@
     [UserId]   INT            IDENTITY (1, 1) NOT NULL,
     [Name]     NVARCHAR (50)  NOT NULL,
     [Password] NVARCHAR (MAX) NOT NULL,
+    [IsActive] BIT            NULL,
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC)
 );
+
+
 
