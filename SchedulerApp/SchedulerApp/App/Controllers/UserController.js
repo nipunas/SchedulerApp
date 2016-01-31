7877fb0ffd7d7bd8872ca2852b07a0180a2b74d1
nipunas <nipuna77@gmail.com>
2016-01-31 10:26:29 +0530

    Changing name of the controller

---
+++ SchedulerApp/SchedulerApp/App/Controllers/UserController.js
@@ -0,0 +1,65 @@
+﻿(function () {
+
+    var app = angular.module('SchedulerApp');
+
+    var LoginController = function ($scope, HttpService, $window) {
+
+        var onSuccess = function (data) {
+            if (data === true) {
+                goToApp();
+            }
+            else {
+                alert('Not Logged in');
+            }
+        },
+        goToApp = function () {
+            sessionStorage.loggedIn = true;
+            sessionStorage.cookie = "1234567890";
+            $window.location.href = "app.html";
+        };
+
+        $scope.user = {
+            Name: '',
+            Password: ''
+        };
+
+        $scope.login = function () {
+            HttpService.login($scope.user)
+            .then(onSuccess);
+        }
+    };
+
+    app.controller("LoginController", ["$scope", "HttpService", "$window", LoginController]);
+
+    //----------------------------------------------------------------
+
+    var SignupController = function ($scope, HttpService, $window) {
+
+        var onSuccess = function (data) {
+            if (data === true) {
+                goToApp();
+            }
+            else {
+                alert('Not Logged in');
+            }
+        },
+        goToApp = function () {
+            sessionStorage.loggedIn = true;
+            sessionStorage.cookie = "1234567890";
+            $window.location.href = "app.html";
+        };
+
+        $scope.user = {
+            Name: '',
+            Password: ''
+        };
+
+        $scope.login = function () {
+            HttpService.login($scope.user)
+            .then(onSuccess);
+        }
+    };
+
+    app.controller("SignupController", ["$scope", "HttpService", "$window", SignupController]);
+
+}());
